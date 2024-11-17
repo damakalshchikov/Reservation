40213c0986956be78615792d8360fe994506c690
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-17 04:30:37 +0300

    Валидация возраста арендатора

--- reservation_project/descriptors/tenant_descriptor.py
+++ reservation_project/descriptors/tenant_descriptor.py
@@ -1,4 +1,11 @@
+from validation.tenant_validation import validate_age
+
+
 class TenantDescriptor:
+    # Минимальный и максимальный возрасты для заключения договора
+    MIN_AGE: int = 18
+    MAX_AGE: int = 99
+
     def __set_name__(self, owner, name):
         self.name = "_" + name
 
@@ -6,4 +13,6 @@ class TenantDescriptor:
         return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
+        if self.name == "_age":
+            validate_age(value, TenantDescriptor.MIN_AGE, TenantDescriptor.MAX_AGE)
         instance.__dict__[self.name] = value
