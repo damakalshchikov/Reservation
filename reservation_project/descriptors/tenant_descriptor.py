d839a0d18e02f41fb263972fc0dd0b409541516c
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-17 03:25:35 +0300

    Добавлен дескриптор для создания свойств(property) для класса Tenant

---
+++ reservation_project/descriptors/tenant_descriptor.py
@@ -0,0 +1,9 @@
+class TenantDescriptor:
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
