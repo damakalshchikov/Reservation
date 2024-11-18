f8aa1b97222ddbd57b0d953e1342c1e5d87ccdea
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-19 01:19:27 +0300

    Рефакторинг

--- reservation_project/descriptors/tenant_descriptor.py
+++ reservation_project/descriptors/tenant_descriptor.py
@@ -6,7 +6,6 @@ from exceptions.tenant_exceptions import TypeAgeError, ValueAgeError, TenantName
 class TenantDescriptor:
     """Дескриптор для создания свойста(property) у объектов класса Tenant"""
 
-    # Минимальный и максимальный возрасты для заключения договора
     MIN_AGE: int = 18
     MAX_AGE: int = 99
     FIRST_AND_LAST_NAME_PATTERN: str = r"^[А-я][а-яА-Я]*$"
@@ -14,11 +13,13 @@ class TenantDescriptor:
 
     def validate_name(self, name: str) -> None:
         """Фу-ия валидации имени и фамилии арендатора"""
+
         if not re.match(self.FIRST_AND_LAST_NAME_PATTERN, name):
             raise TenantNameError
 
     def validate_age(self, age: int) -> None:
         """Фу-ия валидации возраста арендатора"""
+
         if not isinstance(age, int):
             raise TypeAgeError
         elif not self.MIN_AGE <= age <= self.MAX_AGE:
@@ -26,6 +27,7 @@ class TenantDescriptor:
 
     def validate_phone_number(self, number: str) -> None:
         """Фу-ия валидации номера телефона арендатора"""
+
         if not re.match(self.PHONE_NUMBER_PATTERN, number):
             raise PhoneNumberError
 
