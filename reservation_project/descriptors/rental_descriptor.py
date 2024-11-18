125094a7d3017ec3f1054309d7cc9842cdab0a35
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-19 01:16:07 +0300

    Фу-ия валидации цены за ночь арендуемого объекта

--- reservation_project/descriptors/rental_descriptor.py
+++ reservation_project/descriptors/rental_descriptor.py
@@ -1,5 +1,8 @@
 class RentalDescriptor:
     """Дескриптор для создания свойста(property) у объектов класса Rentable"""
+    MIN_PRICE: float = 1
+    MAX_PRICE: float = float("inf")
+
     @staticmethod
     def valudate_name(name: str) -> None:
         """Фу-ия валидации названия арендуемого объекта"""
@@ -13,6 +16,14 @@ class RentalDescriptor:
 
         if not isinstance(address, str):
             raise TypeAddressRentalError
+
+    def validate_price(self, price: int | float) -> None:
+        """Фу-ия валидации цены за ночь арендуемого объекта"""
+
+        if not isinstance(price, (int,float)):
+            raise TypePriceError
+        elif not self.MIN_PRICE <= price <= self.MAX_PRICE:
+            raise ValuePriceError(price, self.MIN_PRICE, self.MAX_PRICE)
 
 
     def __set_name__(self, owner, name):
