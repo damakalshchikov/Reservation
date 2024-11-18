7454cf03e9184287a2f1187ad8fc5235f16924a0
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-18 23:10:50 +0300

    Рефакторинг

--- reservation_project/exceptions/rentable_exceptions.py
+++ reservation_project/exceptions/rentable_exceptions.py
@@ -1,5 +1,7 @@
 class FullReservationError(Exception):
-    def __init__(self, rental_name: str) -> None:
+    """Исключение о переполнении арендуемого объекта"""
+
+    def __init__(self, rental_name: str):
         self.rental_name: str = rental_name
 
     def __str__(self) -> str:
