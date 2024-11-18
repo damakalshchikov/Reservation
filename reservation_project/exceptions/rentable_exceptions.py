bb7c99bbdf4d1ab2ffb87eab3142604f1bbf5ede
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-19 01:06:55 +0300

    Добавлено исключение ValuePriceError. Исключение о неверном значении цены за ночь арендуемого объекта

--- reservation_project/exceptions/rentable_exceptions.py
+++ reservation_project/exceptions/rentable_exceptions.py
@@ -19,6 +19,18 @@ class TypePriceError(Exception):
         return "Цена за ночь должна быть целым/дробным числом"
 
 
+class ValuePriceError(Exception):
+    """Исключение о неверном значении цены за ночь арендуемого объекта"""
+
+    def __init__(self, price: float, min_price: float, max_price: float):
+        self.price: float = price
+        self.min_price: float = min_price
+        self.max_price: float = max_price
+
+    def __str__(self) -> str:
+        return f"Недопустимое значение цены: {self.price}. Цена должна быть в диапазоне от {self.min_price} до {self.max_price}"
+
+
 class FullReservationError(Exception):
     """Исключение о переполнении арендуемого объекта"""
 
