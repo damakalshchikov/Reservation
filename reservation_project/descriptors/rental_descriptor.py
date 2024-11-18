08605743f8d1417b231c4c9408d7994bd93cf5b1
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-19 01:14:24 +0300

    Фу-ия валидации названия аврендуемого объекта

--- reservation_project/descriptors/rental_descriptor.py
+++ reservation_project/descriptors/rental_descriptor.py
@@ -1,5 +1,12 @@
 class RentalDescriptor:
     """Дескриптор для создания свойста(property) у объектов класса Rentable"""
+    @staticmethod
+    def valudate_name(name: str) -> None:
+        """Фу-ия валидации названия аврендуемого объекта"""
+
+        if not isinstance(name, str):
+            raise TypeNameRentalError
+
 
     def __set_name__(self, owner, name):
         self.name = "_" + name
