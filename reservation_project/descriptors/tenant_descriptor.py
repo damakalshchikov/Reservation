77d0f9d1f57b1554113a76618e3d143466115270
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-18 23:11:09 +0300

    Рефакторинг

--- reservation_project/descriptors/tenant_descriptor.py
+++ reservation_project/descriptors/tenant_descriptor.py
@@ -4,6 +4,8 @@ from exceptions.tenant_exceptions import TypeAgeError, ValueAgeError, TenantName
 
 
 class TenantDescriptor:
+    """Дескриптор для создания свойста(property) у объектов класса Tenant"""
+
     # Минимальный и максимальный возрасты для заключения договора
     MIN_AGE: int = 18
     MAX_AGE: int = 99
@@ -11,26 +13,29 @@ class TenantDescriptor:
     PHONE_NUMBER_PATTERN: str = r"^\+79\d{9}$|^89\d{9}$"
 
     def validate_name(self, name: str) -> None:
+        """Фу-ия валидации имени и фамилии арендатора"""
         if not re.match(self.FIRST_AND_LAST_NAME_PATTERN, name):
             raise TenantNameError
 
     def validate_age(self, age: int) -> None:
+        """Фу-ия валидации возраста арендатора"""
         if not isinstance(age, int):
             raise TypeAgeError
         elif not self.MIN_AGE <= age <= self.MAX_AGE:
             raise ValueAgeError(age, self.MIN_AGE, self.MAX_AGE)
 
     def validate_phone_number(self, number: str) -> None:
+        """Фу-ия валидации номера телефона арендатора"""
         if not re.match(self.PHONE_NUMBER_PATTERN, number):
             raise PhoneNumberError
 
-    def __set_name__(self, owner, name) -> None:
+    def __set_name__(self, owner, name):
         self.name = "_" + name
 
-    def __get__(self, instance, owner) -> None:
+    def __get__(self, instance, owner):
         return instance.__dict__[self.name]
 
-    def __set__(self, instance, value: int | str) -> None:
+    def __set__(self, instance, value: int | str):
         match self.name:
             case "_first_name" | "_last_name":
                 self.validate_name(value)
