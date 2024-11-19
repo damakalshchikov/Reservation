1baecffcaf83c77de010a4ecc0eae1bd50bd4b9e
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-19 04:01:29 +0300

    Рефакторинг

--- reservation_project/classes/other/rental_agreement.py
+++ reservation_project/classes/other/rental_agreement.py
@@ -40,7 +40,8 @@ class RentalAgreement:
             tenant.attach_rental(self._rentable)
 
         print(
-            f"""Забронировано {self._guests_count} мест в "{self._rentable.name}" для арендаторов: """
+             f"""Забронировано {self._guests_count} мест в "{self._rentable.name}" """
+            f"""на {self._nights} ночей для арендатора/ов: """
             + ", ".join([f"{t.first_name} {t.last_name}" for t in self._tenants])
         )
 
@@ -53,6 +54,6 @@ class RentalAgreement:
             tenant.detach_rental()
 
         print(
-            f"""Бронирование {self._guests_count} мест в "{self._rentable.name}" отменено для арендаторов: """
+            f"""Бронирование {self._guests_count} мест в "{self._rentable.name}" отменено для арендатора/ов: """
             + ", ".join([f"{t.first_name} {t.last_name}" for t in self._tenants])
         )
