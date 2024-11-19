ba3c1f69f50206dc2bb756ea6680f166ec05c883
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-19 03:57:17 +0300

    Теперь для каждого типа арендуемого объекта задаётся неизменяемая вместимость

--- reservation_project/classes/reservation_objects/rentable.py
+++ reservation_project/classes/reservation_objects/rentable.py
@@ -4,11 +4,13 @@ from classes.abstractions.abstarct_rentable import Rentable
 class Apartment(Rentable):
     """Класс, который представляет квартиру для аренды"""
 
+    def __init__(self, name: str, address: str, price_per_night: int | float):
+        super().__init__(name, address, price_per_night, capacity=4)
+
     @staticmethod
     def from_dict(data: dict) -> "Apartment":
         return Apartment(
             name=data["name"],
             address=data["address"],
-            price_per_night=data["price_per_night"],
-            capacity=data["capacity"],
+            price_per_night=data["price_per_night"]
         )
