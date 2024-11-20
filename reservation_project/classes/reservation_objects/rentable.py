bd9aa66ff97d0cf9027c693538394f7a92cf2ea6
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-20 03:05:53 +0300

    Добавлен класс House, который представляет комнату для сдачи в аренду

--- reservation_project/classes/reservation_objects/rentable.py
+++ reservation_project/classes/reservation_objects/rentable.py
@@ -29,3 +29,18 @@ class Apartment(Rentable):
             address=data["address"],
             price_per_night=data["price_per_night"]
         )
+
+
+class House(Rentable):
+    """Класс, который представляет дом для аренды"""
+
+    def __init__(self, name: str, address: str, price_per_night: int | float):
+        super().__init__(name, address, price_per_night, capacity=8)
+
+    @staticmethod
+    def from_dict(data: dict) -> "House":
+        return House(
+            name=data["name"],
+            address=data["address"],
+            price_per_night=data["price_per_night"]
+        )
