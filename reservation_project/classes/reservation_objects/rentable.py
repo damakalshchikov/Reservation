388f7384b57f506fa6adbaf76243f28331593aa1
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-20 03:05:39 +0300

    Добавлен класс Room, который представляет комнату для сдачи в аренду

--- reservation_project/classes/reservation_objects/rentable.py
+++ reservation_project/classes/reservation_objects/rentable.py
@@ -1,6 +1,21 @@
 from classes.abstractions.abstarct_rentable import Rentable
 
 
+class Room(Rentable):
+    """Класс, который представляет комнату для аренды"""
+
+    def __init__(self, name: str, address: str, price_per_night: int | float):
+        super().__init__(name, address, price_per_night, capacity=2)
+
+    @staticmethod
+    def from_dict(data: dict) -> "Room":
+        return Room(
+            name=data["name"],
+            address=data["address"],
+            price_per_night=data["price_per_night"]
+        )
+
+
 class Apartment(Rentable):
     """Класс, который представляет квартиру для аренды"""
 
