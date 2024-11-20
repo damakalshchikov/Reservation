dee4eedf42ce905a969a70d27a68d3a50da9a247
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2024-11-20 03:04:42 +0300

    Добавлен новый атрибут id у Rentable объектов для использования CRUD-менеджера

--- reservation_project/classes/abstractions/abstarct_rentable.py
+++ reservation_project/classes/abstractions/abstarct_rentable.py
@@ -6,16 +6,24 @@ from descriptors.rental_descriptor import RentalDescriptor
 class Rentable(ABC):
     """Абстрактный класс, который представляет собой арендуемый объект"""
 
+    _next_id: int = 1
+
     name: RentalDescriptor = RentalDescriptor()
     address: RentalDescriptor = RentalDescriptor()
     price_per_night: RentalDescriptor = RentalDescriptor()
 
     def __init__(self, name: str, address: str, price_per_night: float, capacity: int):
+        self._id: int = Rentable._next_id
         self.name: str = name
         self.address: str = address
         self.price_per_night: int | float = price_per_night
         self.__capacity: int = capacity
         self.__available_capacity: int = capacity
+        Rentable._next_id += 1
+
+    @property
+    def rentable_id(self) -> int:
+        return self._id
 
     @property
     def capacity(self) -> int:
